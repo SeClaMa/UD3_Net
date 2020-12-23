d319b7e2beac1eb43df7c671401b405b3e5b4b92
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-23 11:03:54 +0100

    Ejercicio 1 de Unidad 3

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            Console.WriteLine("La suma de los numeros es: {0}", num1 + num2);
+            Console.WriteLine("La resta de los numeros es: {0}", num1 - num2);
+            Console.WriteLine("La multiplicacion de los numeros es: {0}", num1 * num2);
+            Console.WriteLine("La division de los numeros es: {0}", num1 / num2);
+            Console.WriteLine("El modulo de los numeros es: {0}", num1 % num2);
+            Console.ReadKey();
+        }
+    }
+}
