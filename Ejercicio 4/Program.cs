7b2e47870cdedf713605bd8265083cd0582f6816
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-23 12:31:00 +0100

    Ejercicio 4

---
+++ Ejercicio 4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 4;
+            Console.WriteLine("Los resultados son '{0}', '{1}', '{2}'",N=N+77, N=N-3, N=N*2);
+        }
+    }
+}
