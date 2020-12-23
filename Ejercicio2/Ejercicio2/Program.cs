6640bd8e959f73a0178286fac9e9601499699dc5
Locryn <sergiomartinezclavero@hotmail.com>
2020-12-23 12:02:29 +0100

    Ejercicio 2

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 7;
+            double A = 4.53;
+            char C = 'a';
+
+
+            Console.WriteLine("El valor de N es '{0}' ", N);
+            Console.WriteLine("El valor de A es '{0}'", A);
+            Console.WriteLine("El valor de C es '{0}'", C);
+            Console.WriteLine("La suma de N y A es '{0}'", N + A);
+            Console.WriteLine("La diferencia de A y N es '{0}'", A - N);
+            Console.WriteLine("El valor de C es {0}", (int)C);
+        }
+    }
+}
